e20ac00f0923fb01b7ae6667be542cd94bc9218a
r00bal <frontendpit@gmail.com>
2017-11-26 12:35:13 +0100

    Add Create Yelp module. -getAccessToken method calls for the access token -search method retrieves search results form the Yelp API.

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -1,5 +1,65 @@
 import {Client} from './secret.js';
 
 let accessToken;
-const ClientID = Client.Id;
-const ClientSecret = Client.Secret;
+const cors = 'https://cors-anywhere.herokuapp.com/';
+const CLIENT_ID = Client.Id;
+const CLIENT_SECRET = Client.Secret;
+
+const Yelp = {
+  getAccessToken() {
+    if (accessToken) {
+      return new Promise(resolve => resolve(accessToken));
+    };
+    const init = {
+      method:'POST';
+    };
+
+    let urlToFetch = `${cors}https://api.yelp.com/oauth2/token?grant_type=client_credentials`;
+        urlToFetch += `&client_id=${CLIENT_ID}`;
+        urlToFetch += `&client_secret=${CLIENT_SECRET}`;
+    const request = new Request(urlToFetch, init);
+
+    return fetch(request).then(response => {
+      return response.json()
+    }).then(jsonResponse => {
+      accessToken = jsonResponse.access_token;
+    })
+  }
+
+  search(term, location, sortBy) {
+    let urlToFetch = `${cors}https://api.yelp.com/v3/businesses/search`;
+    urlToFetch += `?term=${term}`;
+    urlToFetch += `&location=${location}`;
+    urlToFetch += `&sort_by=${sortBy}`;
+
+    const headers = new Headers({
+     Authorization: `Bearer ${accessToken}`
+   });
+   const init = {
+     headers : headers;
+   }
+   const request = new Request(urlToFetch, init);
+    return Yelp.getAccessToken()
+    .then(() => {
+      return fetch(urlToFetch)
+    }).then(response => {
+      return response.json();
+    }).then(jsonResponse => {
+      console.log(jsonResponse);
+      if (jsonResponse.businesses) {
+        return jsonResponse.businesses.map(business => {
+          id: business.id,
+          imageSrc: business.image_url,
+          name: business.name,
+          address: business.location.address1,
+          city: business.location.city,
+          state: business.location.state,
+          zipCode: business.location.zip_code,
+          category:business.categories[0].title,
+          rating: business.rating,
+          reviewCount: business.review_count
+        });
+      }
+    })
+  }
+}
